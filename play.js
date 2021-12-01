ce2cbf8310661cda39930a7b16210425c5e9ab4d
Michel <Michel_motongi@hotmail.com>
2021-12-01 15:46:10 -0500

    Added change

--- play.js
+++ play.js
@@ -1,6 +1,9 @@
-const net = require("net");
-const connect = require('./client.js')
+const net = require('net');
+const { connect } = require('./client.js');
+const { setupInput } = require("./input.js");
 // establishes a connection with the game server
 
 console.log("Connecting...")
 connect();
+
+setupInput(connect());
