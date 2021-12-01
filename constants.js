6970b8deaca20b80a7f5f87396ac03459ab56cfe
Michel <Michel_motongi@hotmail.com>
2021-12-01 15:45:54 -0500

    Constant file

---
+++ constants.js
@@ -0,0 +1,17 @@
+const IP = '165.227.47.243'
+const PORT = 50541;
+const playerName = "MM"
+const message = 'To infinity… and beyond! '
+const movement = {
+    up: 'w',
+    down: 's',
+    left: 'a',
+    right: 'd'
+}
+module.exports = {
+    IP,
+    PORT,
+    playerName,
+    movement,
+    message
+};
